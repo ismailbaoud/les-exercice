f74290b9891f485f50309578c04e14082f00cb46
ismail baoud <ibaoud46@gmail.com>
2024-10-11 18:50:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# les-exercice
+# les-exercice
---
+++ ex-1.js
@@ -0,0 +1,79 @@
+//exercice 1 :
+
+let prenom=ismail;
+console.log(x);
+
+//exercice 2 :
+
+let a=4;
+let b=8;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//exercice 3 :
+
+let nom="ismail ";
+let prenom="baoud";
+console.log("nom "+"prenom");
+
+//exercice 4 :
+
+let age = 17;
+let majeur ;
+majeur= age>=18;
+console.log(majeur);
+
+//exercice 5 :
+
+let a=4;
+let b=8;
+let c;
+console.log("a=");console.log(a);console.log("b=");console.log(b);
+c=a;
+a=b;
+b=c;
+console.log("a=");console.log(a);console.log("b=");console.log(b);
+
+//exercice 6 :
+
+let rayon = 5;
+let C = 2 * 3.14 * rayon;
+console.log("La circonférence du cercle est : " + C);
+
+//exercice 7:
+
+
+let celsius = 22;
+let fahrenheit = celsius * (9 / 5) + 32;
+console.log(celsius + "°C est équivalent à " + fahrenheit + "°F");
+
+
+
+//exercice 8:
+
+
+let anneeNaissance = 1990;
+let anneeActuelle = 2024;
+let Age = anneeActuelle - anneeNaissance;
+console.log("L'âge de la personne est :", age);
+
+//exercice 9 :
+
+let longueur = 10;
+let largeur = 5;
+let perimetre = 2 * (longueur + largeur);
+console.log("Le périmètre du rectangle est :", perimetre);
+//exercice 10 :
+
+let note1 = 13;
+let note2 = 17;
+let note3 = 12;
+let moyenne = (note1 + note2 + note3) / 3;
+console.log("La moyenne des trois notes est :", moyenne);
+
+
+
+
+
